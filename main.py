d180df623fced528f7f0a594ce4d94ac8d79e8d8
siennawheeler <sienna_wheeler1@my.vcccd.edu>
2024-06-26 21:41:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    numbers[0] = int(input())
+    x = 0
+    while True:
+        temp = int(input())
+        if (temp > numbers[x]):
+            break
+        x = x + 1
+        numbers.append(temp)
     ########################################
     # Do not delete the return statement
     ########################################
